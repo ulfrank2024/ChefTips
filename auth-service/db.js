8f8ddb5c244f34287723ae7c04f9d48f8e3e9a51
ulfrank2024 <2724548@collegelacite.ca>
2025-11-08 02:20:44 -0500

    mise a jour

--- auth-service/db.js
+++ auth-service/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
