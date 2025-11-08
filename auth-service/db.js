a2c7a2aedc86b0df29a3c30bd3653b632c2923a2
ulfrank2024 <2724548@collegelacite.ca>
2025-11-08 02:03:01 -0500

    mise a jour

--- auth-service/db.js
+++ auth-service/db.js
@@ -3,9 +3,6 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Nécessaire pour Render si l'URL ne le gère pas directement
-    }
 });
 
 module.exports = pool;
--- tip-service/config/db.js
+++ tip-service/config/db.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Nécessaire pour Render si l'URL ne le gère pas directement
-    }
 });
 
 module.exports = { pool };
