4f9e0f3019cebc842e08477134e6405e4444d9fb
ulfrank2024 <2724548@collegelacite.ca>
2025-11-08 01:12:53 -0500

    mise a jour

--- tip-service/config/db.js
+++ tip-service/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    // ssl: { rejectUnauthorized: false } // Uncomment for certain cloud providers
+    ssl: { rejectUnauthorized: false } // Activer SSL
 });
 
 module.exports = { pool };
